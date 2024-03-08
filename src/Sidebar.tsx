9d295f4caaf666df3fc5a29edf238184c6ac6c01
Kevin <kevin@kokev.de>
2024-03-08 19:28:50 +0100

    Use a proper key function to fix the dragging issue. However, now we select text after dragging

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -12,11 +12,12 @@ export function Sidebar(props: SidebarProperties) {
   const distance = props.route ? props.route.get_distance() : 0;
 
   // XXX: We need the key to force a remount of the component. Otherwise spring is weird when the props order changes
+  const key = props.waypoints.map((wp) => `${wp.lng()}${wp.lat()}`).join("");
   return (
     <div>
       <label> Total Distance: {(distance / 1000.0).toFixed(2)}km </label>
       <DraggableList
-        key={props.waypoints.map((wp) => `${wp.name}`).join("")}
+        key={key}
         items={props.waypoints.map((wp) => `${wp.name}`)}
         callback_order_change={props.callback_update_wp_order}
       />
