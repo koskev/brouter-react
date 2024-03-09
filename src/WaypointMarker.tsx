73a5332f656816eedfbe1dc0ffffe97559315ad7
Kevin <kevin@kokev.de>
2024-03-09 15:47:42 +0100

    Use an own svg file for markers. Allows for better numbers and other stuff

--- src/WaypointMarker.tsx
+++ src/WaypointMarker.tsx
@@ -3,6 +3,7 @@ import { Waypoint } from "./GeoSegment";
 import { Marker, Popup } from "react-leaflet";
 import L, { LatLng } from "leaflet";
 import { callbacks_waypoint } from "./utils/callbacks";
+import marker_svg_string from "./marker.svg?raw";
 
 export interface MarkerProperties {
   position: Waypoint;
@@ -34,20 +35,30 @@ export function WaypointMarker(props: MarkerProperties) {
     }
   };
 
-  const color = props.position.highlight ? "0000ff" : "fe4848";
-  // from https://github.com/tomickigrzegorz/react-leaflet-examples/blob/main/src/pages/svg-markers-width-legends.js MIT
-  const svgTemplate = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker">
-    <path fill-opacity=".25" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/>
-    <path fill="#${color}" stroke="#fff" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/>
-    <text x="14" y="12.5" fill="black" stroke="none" font-size="8">${props.position.get_number() + 1}</text>
-    </svg>`;
+  const color = props.position.highlight ? "#0000ff" : "#fe4848";
+
+  let svg_element = new DOMParser().parseFromString(
+    marker_svg_string,
+    "image/svg+xml",
+  );
+
+  let text_element = svg_element.getElementById("marker_text");
+  if (text_element) {
+    text_element.innerHTML = `${props.position.get_number() + 1}`;
+  }
+
+  let svg_html = svg_element.activeElement as HTMLElement;
+
+  let marker_paths = svg_element.getElementById("marker_paths");
+  if (marker_paths) {
+    marker_paths.style.setProperty("--fill-color", color);
+  }
 
   let icon = new L.DivIcon({
     className: "test",
-    html: svgTemplate,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+    html: svg_html,
+    iconSize: [20, 70],
+    iconAnchor: [20, 70],
     popupAnchor: [0, -40],
   });
 
