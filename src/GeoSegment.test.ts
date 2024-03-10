7dd44d65d5660a59d19c4901ad38fcdbb181f4f5
Kevin <kevin@kokev.de>
2024-03-10 22:31:41 +0100

    Fix test

--- src/GeoSegment.test.ts
+++ src/GeoSegment.test.ts
@@ -12,10 +12,10 @@ test("brouter parse", () => {
   const geo_route = new GeoRoute(brouter_array);
   expect(geo_route.segments.length).toBe(2);
   expect(geo_route.segments[0].points.length).toBe(3);
-  expect(geo_route.segments[0].elevation).toBe(15);
-  expect(geo_route.segments[0].distance).toBe(65);
+  expect(geo_route.segments[0].message.elevation).toBe(15);
+  expect(geo_route.segments[0].message.distance).toBe(65);
   expect(geo_route.segments[1].points.length).toBe(2);
   expect(geo_route.segments[1].points[0][0]).toBe(10.087361);
-  expect(geo_route.segments[1].elevation).toBe(15);
-  expect(geo_route.segments[1].distance).toBe(3);
+  expect(geo_route.segments[1].message.elevation).toBe(15);
+  expect(geo_route.segments[1].message.distance).toBe(3);
 });
